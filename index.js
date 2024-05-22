81e758a4d9f4c2d4d6e634fbde45e871c624df61
Zeus <purslh1@unlv.nevada.edu>
2024-05-21 20:26:10 -0700

    0.0.4 worked on buildMap function and shuffled items around

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // Obtain user current location | status: completed
 // Map location on Leaflet map | status: completed
 // Allow user to select a business type from a list and map five nearest locations on the map using Foursquare API | status: completed
+const myMap = {
+    coords: [],
+    businesses: [],
+    map: {},
+    markers: {},
+
+    buildMap(){
+        this.map = L.map('map', {
+            center: this.coords,
+            zoom: 14,
+        });
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            minZoom: '15',
+        }).addTo(this.map)
+        const marker = L.marker([this.coords])
+        marker
+        .addTo(this.map)
+        .bindPopup('You are here!')
+        .openPopup()
+    }
+}
 function geoFindMe(){
     const status = document.querySelector("#status");
     const mapLink = document.querySelector("#map-link");
@@ -27,12 +49,9 @@ function geoFindMe(){
     const watchID = navigator.geolocation.watchPosition(success, error, options);
 }
 
-var map = L.map('map').setView([latitude, longitude], 13);
+var map = L.map('map').setView([latitude, longitude], 14);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([latitude, longitude]).addTo(map)
-    .bindPopup('You are here!')
-    .openPopup();
