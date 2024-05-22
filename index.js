9c75dd1b5b5544ca8924602a238a302d2286c136
Zeus <purslh1@unlv.nevada.edu>
2024-05-21 20:15:17 -0700

    0.0.3 implemented leaflet functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// Obtain user current location
-// Map location on Leaflet map
-// Allow user to select a business type from a list and map five nearest locations on the map using Foursquare API
+// Obtain user current location | status: completed
+// Map location on Leaflet map | status: completed
+// Allow user to select a business type from a list and map five nearest locations on the map using Foursquare API | status: completed
 function geoFindMe(){
     const status = document.querySelector("#status");
     const mapLink = document.querySelector("#map-link");
@@ -26,3 +26,13 @@ function geoFindMe(){
     };
     const watchID = navigator.geolocation.watchPosition(success, error, options);
 }
+
+var map = L.map('map').setView([latitude, longitude], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([latitude, longitude]).addTo(map)
+    .bindPopup('You are here!')
+    .openPopup();
